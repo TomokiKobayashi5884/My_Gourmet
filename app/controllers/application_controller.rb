42370e9e807f313deec1ad40ee9e1af2792f7c51
TomokiKobayashi <tomoki58840725@gmail.com>
2021-06-11 03:36:42 +0000

    01 create_toppage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+    
+    def index
+    end
+    
+    def show
+    end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    
 end
