ebb79f899f4a06e2045fc62e8570e2f6cc78ee46
TomokiKobayashi <tomoki58840725@gmail.com>
2021-07-16 02:10:44 +0000

    setting_S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = ENV['BACKET_NAME'] # バケット名
-    config.fog_public = false
+    config.fog_public = true
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['ACCESS_KEY_ID'], # アクセスキー
