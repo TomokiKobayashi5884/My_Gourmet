c381db16758eede5cb633557dbb96a30b168b0c5
TomokiKobayashi <tomoki58840725@gmail.com>
2021-07-16 12:54:46 +0000

    setting_S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,17 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = ENV['BACKET_NAME'] # バケット名
-    config.fog_public = false
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['ACCESS_KEY_ID'], # アクセスキー
-      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'], # シークレットアクセスキー
-      region: ENV['REGION'], # リージョン
-      path_style: true
-    }
-end
+# CarrierWave.configure do |config|
+#     config.storage :fog
+#     config.fog_provider = 'fog/aws'
+#     config.fog_directory  = ENV['BACKET_NAME'] # バケット名
+#     config.fog_public = false
+#     config.fog_credentials = {
+#       provider: 'AWS',
+#       aws_access_key_id: ENV['ACCESS_KEY_ID'], # アクセスキー
+#       aws_secret_access_key: ENV['SECRET_ACCESS_KEY'], # シークレットアクセスキー
+#       region: ENV['REGION'], # リージョン
+#       path_style: true
+#     }
+# end
