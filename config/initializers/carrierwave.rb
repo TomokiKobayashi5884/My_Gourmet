0a4b272c490595010b6b5355d8d7a6f791a81408
TomokiKobayashi <tomoki58840725@gmail.com>
2021-07-17 00:54:30 +0000

    setting_S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION'],
+    region: ap-northeast-1,
     path_style: true
   }
 end
