a03930358492ee78974a9fdf700e9d36b5f52cfb
TomokiKobayashi <tomoki58840725@gmail.com>
2021-07-17 01:45:56 +0000

    setting_S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = ENV['BACKET']
+  config.fog_directory  = ENV['BUCKET']
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
